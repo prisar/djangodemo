e12643bccb151011fa8a5bf2cb437a224f49fa74
biyorn <pritimaysarkar2010@gmail.com>
2018-09-15 06:47:01 +0000

    create static folder

---
+++ scrumboard/static/scrumboard/scrumboard.js
@@ -0,0 +1,41 @@
+(function() {
+    'use strict';
+
+    angular.module('scrumboard.demo', [])
+        .controller('ScrumboardController', ['$scope', ScrumboardController]);
+
+    function ScrumboardController($scope) {
+        $scope.add = function(list, title) {
+            var card = {
+                title: title
+            };
+
+            list.cards.push(card);
+        }
+
+        $scope.data = [{
+                name: 'Django demo',
+                cards: [{
+                        title: 'Create Models'
+                    },
+                    {
+                        title: 'Create View'
+                    },
+                    {
+                        title: 'Migrate Database'
+                    }
+                ]
+            },
+            {
+                name: 'Angluar demo',
+                cards: [{
+                        title: 'Write html'
+                    },
+                    {
+                        title: 'Write javascript'
+                    }
+                ]
+            }
+        ];
+    }
+}());
